2018991b40130838f71db663d9a856c518b33e8a
rasulov-e <rasulov_e@auca.kg>
2021-02-11 20:07:31 +0600

    newsfeed: Get function

--- platform/newsfeed/newsfeed.go
+++ platform/newsfeed/newsfeed.go
@@ -10,6 +10,32 @@ type Feed struct {
 	DB *sql.DB
 }
 
+func (feed *Feed) Get() ([]Item, error) {
+	items := []Item{}
+
+	rows, err := feed.DB.Query(`
+		SELECT * FROM newsfeed
+	`)
+
+	if err != nil {
+		log.Printf("newsfeed: NewFeed: %s", err.Error())
+		return nil, err
+	}
+
+	var id int
+	var content string
+
+	for rows.Next() {
+		rows.Scan(&id, &content)
+		item := Item{
+			ID:      id,
+			Content: content,
+		}
+		items = append(items, item)
+	}
+	return items, nil
+
+}
 func (feed *Feed) Add(item Item) (int64, error) {
 	stmt, _ := feed.DB.Prepare(`
 		INSERT INTO newsfeed (content) values (?)
@@ -18,7 +44,7 @@ func (feed *Feed) Add(item Item) (int64, error) {
 	res, err := stmt.Exec(item.Content)
 	id, _ := res.LastInsertId()
 
-	if err != nil && id > 0 {
+	if err != nil || id == 0 {
 		log.Printf("newsfeed: Add: error")
 		return id, err
 	}
@@ -35,16 +61,17 @@ func NewFeed(db *sql.DB) (*Feed, error) {
 	`)
 	if err != nil {
 		log.Printf("newsfeed: NewFeed: %s", err.Error())
+		return nil, err
 	}
 
-	res, err := stmt.Exec()
-	id, _ := res.LastInsertId()
+	_, err = stmt.Exec()
 
-	if err != nil && id > 0 {
+	if err != nil {
 		log.Printf("newsfeed: NewFeed: %s", err.Error())
+		return nil, err
 	}
 
 	return &Feed{
 		DB: db,
-	}, err
+	}, nil
 }
